c2b9af5ae030928f9326951c7ae1845fcd46b9c5
S.Takahashi <36907744+syoukitakahashi@users.noreply.github.com>
2021-10-26 19:32:24 +0900

    Update musicpage1.js

--- musicpage1.js
+++ musicpage1.js
@@ -69,6 +69,8 @@
         });
         
         document.getElementById("btn_play").addEventListener("click",()=>{
+          if(audioctx.state=="suspended")
+            audioctx.resume();            
           if( ! bgm.paused ){
               bgm.pause();
               stopTimer();
