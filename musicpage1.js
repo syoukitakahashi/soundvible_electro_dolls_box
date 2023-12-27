280253e746af66ad521a117cc8acaf8aebd65e77
S.Takahashi <36907744+syoukitakahashi@users.noreply.github.com>
2023-12-27 19:18:50 +0900

    Update musicpage1.js

--- musicpage1.js
+++ musicpage1.js
@@ -1,22 +1,14 @@
     window.addEventListener("load", ()=>{
-
-        const audioElement = document.querySelector('input[type="file"]').files;
-        // ファイルの名前を取得する
-        const fileName = audioElement.name;
-        // ファイルのサイズを取得する
-        const fileSize = audioElement.size;
-        // ファイルのデータを取得する
-        //const fileData = await audioElement.arrayBuffer();
-
-        // ファイルのデータを Web Audio API で読み込む
-        const audioctx = new AudioContext();
-        const audioBuffer = audioContext.createBuffer(2, 44100, 'float32');
-        audioBuffer.copyFrom(fileData);
-        const audioBufferSourceNode = audioctx.createBufferSource();
-        audioBufferSourceNode.buffer = audioBuffer;
-        
+        const audioctx = new AudioContext();          
+        const audioElement = document.querySelector('#bgm');
         var bgm = audioctx.createMediaElementSource(audioElement);
         let mode = 0;
+        const btn_loop = document.querySelector("#btn_loop");
+        const btn_vo = document.querySelector("#btn_vo");
+        const vosl = document.querySelector("#volumesl");
+        const playback_position = document.getElementById("playback_position");
+        const end_position = document.getElementById("end_position");
+        const slider_progress = document.getElementById("progress");
         
         const gainvol = new GainNode(audioctx,{gain:0.7});
         const analyser = new AnalyserNode(audioctx, {smoothingTimeConstant:0.2});            
